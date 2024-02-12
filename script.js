284857449b0ceb39fe25fca323049b98f9fffc46
Oxide <mohammedhaider15mo@gmail.com>
2024-02-12 13:01:34 +0300

    added the ability to change size and improved the styling

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const container = document.querySelector("#grid-container");
 const widthinput = document.querySelector(".width");
 const heightinput = document.querySelector(".height");
 const enterbtn = document.querySelector(".enterbtn");
+const eraser = document.querySelector("#eraserchbx");
+
+const DRAWCOLOR = "background-color: black";
+const ERASECOLOR = "background-color: transparent";
+
+let pixels = new Array();
 
 enterbtn.addEventListener("click", Start)
 
@@ -16,8 +22,9 @@ function setDefaultValues()
 
 function Start()
 {
-    const width = widthinput.value;
-    const height = heightinput.value;
+    const width = Number(widthinput.value);
+    const height = Number(heightinput.value);
+
     ClearGrid();
     GenerateGrid(width, height)
 }
@@ -35,7 +42,37 @@ function GenerateGrid(width, height)
             let vGrid = document.createElement("div");
             vGrid.classList.add("v-grid");
             hGrid.appendChild(vGrid);
+            pixels.push(hGrid);
         }
+    }
+    addEventListener();
+}
+
+function addEventListener()
+{
+    for(let i = 0; i < pixels.length; i++)
+    {
+        if(pixels[i])
+        pixels[i].addEventListener
+        ("mouseover", (Event) => 
+        {
+            if(mouseDown)
+            color(Event);
+        });
+        pixels[i].addEventListener("mousedown", color);
+    }
+}
+
+function color(Event)
+{
+    let erasing = eraser.checked;
+    if(erasing)
+    {
+        Event.target.style = ERASECOLOR;
+    }
+    else
+    {
+        Event.target.style = DRAWCOLOR;
     }
 }
 
@@ -43,6 +80,14 @@ function ClearGrid()
 {
     while(container.firstChild != null)
     {
-        container.removeChild(container.firstChild);
+        container.firstChild.remove();
     }
+}
+
+let mouseDown = false;
+document.body.onmousedown = function() { 
+  mouseDown = true;
+}
+document.body.onmouseup = function() {
+  mouseDown = false;
 }
